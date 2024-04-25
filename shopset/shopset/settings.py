56df58edd3bebcb92d2f8de33db393a91847dfb6
Pranoza <ahp.mee@gmail.com>
2024-04-25 14:27:10 +0330

    fix errors

--- shopset/shopset/settings.py
+++ shopset/shopset/settings.py
@@ -37,10 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "crispy_forms",
-    'bootstrap5',
     'main.apps.MainConfig',
-    'register.apps.RegisterConfig',
+
 ]
 
 MIDDLEWARE = [
--- shopset/shopset/urls.py
+++ shopset/shopset/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from register import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("register/", v.register, name="register"),  
     path('', include('main.urls')), # main will be the name of your app
 ]
